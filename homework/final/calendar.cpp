1fa458c798e2e2b540a701afa8f213aaa63787cd
Spencer Owens <thespencerowens@outlook.com>
2023-10-15 11:20:01 -0800

    20231015: LC Final Project Start

---
+++ homework/final/calendar.cpp
@@ -0,0 +1,15 @@
+/* 	
+;		Name: Spencer Owens
+;		Class: F23 CS F301
+; 	Date Created: 2023-10-11
+; 	Date Updated: 2023-10-11
+;		Program Name: calendar.cpp
+;		Program Function: 
+*/
+
+#include <iostream>
+
+int main () {
+  std::cout << "Hello\n";
+  return 0;
+}
